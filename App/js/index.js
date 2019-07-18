922276577a1d5fa21cb8349a1b4a439765042ec9
rasden <servis@consilium.sk>
2019-07-18 15:00:19 +0200

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,142 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.CheckBox")
+                .setHost(host,"checkMQ")
+                .setDirtyMark(false)
+                .setLeft("8.333333333333334em")
+                .setTop("18.333333333333332em")
+                .setWidth("12em")
+                .setCaption("MQ")
+            );
+            
+            append(
+                xui.create("xui.UI.CheckBox")
+                .setHost(host,"checkOPQ")
+                .setDirtyMark(false)
+                .setLeft("8.333333333333334em")
+                .setTop("21.666666666666668em")
+                .setWidth("12em")
+                .setCaption("OPQ")
+            );
+            
+            append(
+                xui.create("xui.UI.CheckBox")
+                .setHost(host,"checkUCR")
+                .setDirtyMark(false)
+                .setLeft("8.333333333333334em")
+                .setTop("25.833333333333332em")
+                .setWidth("12em")
+                .setCaption("UCR")
+            );
+            
+            append(
+                xui.create("xui.UI.ComboInput")
+                .setHost(host,"xui_ui_comboinput3")
+                .setDirtyMark(false)
+                .setLeft("43.333333333333336em")
+                .setTop("12.5em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("Date Picker")
+                .setType("date")
+            );
+            
+            append(
+                xui.create("xui.UI.DatePicker")
+                .setHost(host,"xui_ui_datepicker1")
+                .setDirtyMark(false)
+                .setLeft("40em")
+                .setTop("15.833333333333334em")
+                .setValue(new Date(2019,6,18,0,0,0,0))
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"InputName")
+                .setRequired(true)
+                .setDirtyMark(false)
+                .setLeft("2.5em")
+                .setTop("3.3333333333333335em")
+                .setWidth("58.833333333333336em")
+                .setHeight("2.5em")
+                .setLabelSize("10em")
+                .setLabelCaption("Meno a priezvisko")
+                .setMultiLines(true)
+            );
+            
+            append(
+                xui.create("xui.UI.ComboInput")
+                .setHost(host,"comboCompany")
+                .setDirtyMark(false)
+                .setLeft("5em")
+                .setTop("7.5em")
+                .setWidth("30.5em")
+                .setLabelSize("8em")
+                .setLabelCaption("Spoločnosť")
+                .setType("listbox")
+                .setItems([
+                    {
+                        "id":"a",
+                        "caption":"item 1",
+                        "imageClass":"xui-icon-number1"
+                    },
+                    {
+                        "id":"b",
+                        "caption":"item 2",
+                        "imageClass":"xui-icon-number2"
+                    },
+                    {
+                        "id":"c",
+                        "caption":"item 3",
+                        "imageClass":"xui-icon-number3"
+                    },
+                    {
+                        "id":"d",
+                        "caption":"item 4",
+                        "imageClass":"xui-icon-number4",
+                        "disabled":true
+                    }
+                ])
+            );
+            
+            append(
+                xui.create("xui.UI.ComboInput")
+                .setHost(host,"comboUser")
+                .setDirtyMark(false)
+                .setLeft("5em")
+                .setTop("10.833333333333334em")
+                .setWidth("30.5em")
+                .setLabelSize("8em")
+                .setLabelCaption("Drop List Input")
+                .setType("listbox")
+                .setItems([
+                    {
+                        "id":"a",
+                        "caption":"item 1",
+                        "imageClass":"xui-icon-number1"
+                    },
+                    {
+                        "id":"b",
+                        "caption":"item 2",
+                        "imageClass":"xui-icon-number2"
+                    },
+                    {
+                        "id":"c",
+                        "caption":"item 3",
+                        "imageClass":"xui-icon-number3"
+                    },
+                    {
+                        "id":"d",
+                        "caption":"item 4",
+                        "imageClass":"xui-icon-number4",
+                        "disabled":true
+                    }
+                ])
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
